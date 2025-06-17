9a23bff3656d3214f8a231edf40b9dae6784a498
Aeltumn <daniel@goossens.ch>
2025-06-18 00:59:34 +0200

    Update to Minecraft 1.21.6

--- src/main/java/com/aeltumn/togglesprintdisplay/mixin/GuiMixin.java
+++ src/main/java/com/aeltumn/togglesprintdisplay/mixin/GuiMixin.java
@@ -5,10 +5,8 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.Font;
 import net.minecraft.client.gui.Gui;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.gui.LayeredDraw;
 import net.minecraft.client.gui.components.DebugScreenOverlay;
 import net.minecraft.network.chat.Component;
-import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
@@ -20,30 +18,26 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class GuiMixin {
 
     @Shadow
-    @Final
-    private LayeredDraw layers;
-
-    @Shadow
     public abstract DebugScreenOverlay getDebugOverlay();
 
     @Shadow
     public abstract Font getFont();
 
-    @Inject(method = "<init>", at = @At("TAIL"))
-    public void onInit(Minecraft minecraft, CallbackInfo ci) {
-        var overlay = new LayeredDraw();
-        overlay.add(this::tsd$renderSprintingOverlay);
-        this.layers.add(overlay, () ->
-            // Check that the main GUI is not hidden
-            !minecraft.options.hideGui &&
-                // Check that the debug screen is not up
-                !this.getDebugOverlay().showDebugScreen()
-        );
+    @Inject(method = "render", at = @At("TAIL"))
+    public void render(GuiGraphics graphics, DeltaTracker deltaTracker, CallbackInfo ci) {
+        tsd$renderSprintingOverlay(graphics);
     }
 
     @Unique
-    private void tsd$renderSprintingOverlay(GuiGraphics guiGraphics, DeltaTracker deltaTracker) {
+    private void tsd$renderSprintingOverlay(GuiGraphics guiGraphics) {
         var minecraft = Minecraft.getInstance();
+
+        // Check that the main GUI is not hidden
+        if (minecraft.options.hideGui) return;
+
+        // Check that the debug screen is not up
+        if (this.getDebugOverlay().showDebugScreen()) return;
+
         var isSprinting = minecraft.options.keySprint.isDown();
         var toggleSprint = minecraft.options.toggleSprint().get();
         var text = Component.translatable("menu.toggle_sprint_display." + (toggleSprint ? "toggle" : "hold") + "_" + (isSprinting ? "on" : "off"));
@@ -51,8 +45,8 @@ public abstract class GuiMixin {
         // If there is no text to be drawn, do nothing!
         if (text.getString().isEmpty()) return;
 
-        var k = 16777215;
         var font = getFont();
-        guiGraphics.drawString(font, text, 7, 7, k);
+        guiGraphics.nextStratum();
+        guiGraphics.drawString(font, text, 7, 7, -1);
     }
 }
